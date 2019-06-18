ded30fb56813e7837daf7cd0d9018086f2f07dbe
kmayerb <kmayerb@gmail.com>
2019-06-18 12:55:45 -0700

    fix carve test

--- meleagris/tests/test_carve.py
+++ meleagris/tests/test_carve.py
@@ -5,7 +5,7 @@ def test_carve0():
 	npt.assert_equal(carve.carve(1,5), 6)
 
 def test_carve1():
-	npt.assert_equal(carve.carve(1,1), 4)
+	npt.assert_equal(carve.carve(1,1), 2)
 
 def test_carve2():
 	npt.assert_equal(carve.carve(1,0), 1)
