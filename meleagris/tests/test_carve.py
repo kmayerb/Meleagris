1ae8504ff105fca49af9b0fae5db801135fc53e1
kmayerb <kmayerb@gmail.com>
2019-06-18 12:30:21 -0700

    add a test_carve test

--- meleagris/tests/test_carve.py
+++ meleagris/tests/test_carve.py
@@ -1,8 +1,11 @@
 import numpy.testing as npt
 from meleagris import carve
 
-def test_carve1():
+def test_carve0():
 	npt.assert_equal(carve.carve(1,5), 6)
+
+def test_carve1():
+	npt.assert_equal(carve.carve(1,1), 2)
 
 def test_carve2():
 	npt.assert_equal(carve.carve(1,0), 1)
